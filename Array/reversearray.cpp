e080221651a2cc23fa6c2116d0cc16a1fc998398
Its-Seeker <ketanarora482@gmail.com>
2026-09-02 01:02:14 +0530

    Add functions for finding minimum and maximum values in an array

---
+++ Array/reversearray.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+void reverse(int arr[],int n){
+  //array indexing start 0 and eng n-1, [5]= 0 to 4;  
+  int start=0;
+  int end=n-1;
+  while(start<=end){
+    swap(arr[start],arr[end]);
+    start++;
+    end--;
+  }
+}
+void printArray(int arr[],int n){
+for(int i=0;i<n;i++){
+  cout<<arr[i]<<" ";
+}
+cout<<endl;
+}
+
+int main(){
+int arr[6]={5,7,12,-9,0,6};
+int brr[5]={2,4,6,5,8};
+
+reverse(arr,6);
+reverse(brr,5);
+
+printArray(arr,6);
+printArray(brr,5);
+
+ return 0;
+}
